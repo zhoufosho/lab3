98ebbc31e452f901054e95ab312ea28ac9f0aa83
Elaine Zhou <ezhou@stanford.edu>
2014-01-22 15:13:35 -0800

    add project click interactivity. finish lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,38 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("hooray!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(submitForm);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       description.toggle();
+    }
+}
+
+function submitForm(e){
+	var projectId = $("#project").val();
+	$(projectId).animate({
+		width: $('#width').val()
+	})
+	var newDescription = $('#description').val();
+	$(projectId + " .project-description").text(newDescription);
 }
